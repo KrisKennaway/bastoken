3558322cf7bbe553c4afde9b3499ba96d998c4a7
kris <kris.kennaway@gmail.com>
2020-09-26 21:47:26 +0100

    Clean up a bit

--- bastoken.py
+++ bastoken.py
@@ -10,132 +10,46 @@ import sys
 TOKENS = {}
 
 for i, t in enumerate([
-    "END",
-    "FOR",
-    "NEXT",
-    "DATA",
-    "INPUT",
-    "DEL",
-    "DIM",
-    "READ",
-    "GR",
-    "TEXT",
-    "PR#",
-    "IN#",
-    "CALL",
-    "PLOT",
-    "HLIN",
-    "VLIN",
-    "HGR2",
-    "HGR",
-    "HCOLOR=",
-    "HPLOT",
-    "DRAW",
-    "XDRAW",
-    "HTAB",
-    "HOME",
-    "ROT=",
-    "SCALE=",
-    "SHLOAD",
-    "TRACE",
-    "NOTRACE",
-    "NORMAL",
-    "INVERSE",
-    "FLASH",
-    "COLOR",
-    "POP",
-    "VTAB",
-    "HIMEM:",
-    "LOMEM:",
-    "ONERR",
-    "RESUME",
-    "RECALL",
-    "STORE",
-    "SPEED=",
-    "LET",
-    "GOTO",
-    "RUN",
-    "IF",
-    "RESTORE",
-    "&",
-    "GOSUB",
-    "RETURN",
-    "REM",
-    "STOP",
-    "ON",
-    "WAIT",
-    "LOAD",
-    "SAVE",
-    "DEF",
-    "POKE",
-    "PRINT",
-    "CONT",
-    "LIST",
-    "CLEAR",
-    "GET",
-    "NEW",
-    "TAB(",
-    "TO",
-    "FN",
-    "SPC(",
-    "THEN",
-    "AT",
-    "NOT",
-    "STEP",
-    "+",
-    "-",
-    "*",
-    "/",
-    "^",
-    "AND",
-    "OR",
-    ">",
-    "=",
-    "<",
-    "SGN",
-    "INT",
-    "ABS",
-    "USR",
-    "FRE",
-    "SCRN(",
-    "PDL",
-    "POS",
-    "SQR",
-    "RND",
-    "LOG",
-    "EXP",
-    "COS",
-    "SIN",
-    "TAN",
-    "ATN",
-    "PEEK",
-    "LEN",
-    "STR$",
-    "VAL",
-    "ASC",
-    "CHR$",
-    "LEFT$",
-    "RIGHT$",
-    "MID$"
-]):
-    TOKENS[t] = 0x80+i
+    "END", "FOR", "NEXT", "DATA", "INPUT", "DEL", "DIM", "READ", "GR", "TEXT",
+    "PR#", "IN#", "CALL", "PLOT", "HLIN", "VLIN", "HGR2", "HGR", "HCOLOR=",
+    "HPLOT", "DRAW", "XDRAW", "HTAB", "HOME", "ROT=", "SCALE=", "SHLOAD",
+    "TRACE", "NOTRACE", "NORMAL", "INVERSE", "FLASH", "COLOR", "POP", "VTAB",
+    "HIMEM:", "LOMEM:", "ONERR", "RESUME", "RECALL", "STORE", "SPEED=", "LET",
+    "GOTO", "RUN", "IF", "RESTORE", "&", "GOSUB", "RETURN", "REM", "STOP", "ON",
+    "WAIT", "LOAD", "SAVE", "DEF", "POKE", "PRINT", "CONT", "LIST", "CLEAR",
+    "GET", "NEW", "TAB(", "TO", "FN", "SPC(", "THEN", "AT", "NOT", "STEP", "+",
+    "-", "*", "/", "^", "AND", "OR", ">", "=", "<", "SGN", "INT", "ABS", "USR",
+    "FRE", "SCRN(", "PDL", "POS", "SQR", "RND", "LOG", "EXP", "COS", "SIN",
+    "TAN", "ATN", "PEEK", "LEN", "STR$", "VAL", "ASC", "CHR$", "LEFT$",
+    "RIGHT$", "MID$"]):
+    TOKENS[t] = 0x80 + i
 
-def parse_program(lines):
+
+def tokenize_program(lines):
+    """Tokenizes a program consisting of multiple lines."""
+
     yield 0x00
     addr = 0x801
     for line in lines:
-        linenum, tokenized = parse_line(line.rstrip("\n"))
+        linenum, tokenized = tokenize_line(line.rstrip("\n"))
         tokenized = list(tokenized)
         addr += len(tokenized) + 4
+        # Starting address of next program line (or EOF)
         yield addr % 256
         yield int(addr / 256)
+        # Line number
         yield linenum % 256
         yield int(linenum / 256)
+        # Statement body
         yield from tokenized
+    # No more lines
     yield 0x00
     yield 0x00
 
-def parse_line(line):
+
+def tokenize_line(line: str):
+    """Tokenizes a program line consisting of line number and statement body."""
+
     line_num_str = ""
     for idx, char in enumerate(line):
         if char >= '0' and char <= '9':
@@ -146,31 +60,34 @@ def parse_line(line):
     if line_num > 65535:
         raise ValueError(line_num)
 
-    return int(line_num), parse_statements(line[idx:])
+    return int(line_num), tokenize_statements(line[idx:])
 
-def parse_statements(line):
-    dataFlag = False
-    remFlag = False
-    quoteFlag = None
+
+def tokenize_statements(line: str):
+    """Emits sequence of tokens for a line statement body."""
+
+    data_mode = False  # Are we inside a DATA statement?
+    rem_mode = False  # Are we inside a REM statement?
+    quote_mode = False  # Are we inside a quoted string?
 
     char_idx = 0
     while char_idx < len(line):
         char = line[char_idx]
 
-        if char == " " and not (dataFlag or remFlag or quoteFlag):
+        if char == " " and not (data_mode or rem_mode or quote_mode):
             char_idx += 1
             continue
 
         if char == '"':
-            quoteFlag = not quoteFlag
+            quote_mode = not quote_mode
 
         if char == ':':
-            dataFlag = False
+            data_mode = False
             yield ord(char)
             char_idx += 1
             continue
 
-        if quoteFlag or remFlag or dataFlag:
+        if quote_mode or rem_mode or data_mode:
             yield ord(char)
             char_idx += 1
             continue
@@ -188,14 +105,15 @@ def parse_statements(line):
         tokens, char_idx = read_token(line, char_idx)
         for token in tokens:
             if token == TOKENS["DATA"]:
-                dataFlag = True
+                data_mode = True
             elif token == TOKENS["REM"]:
-                remFlag = True
+                rem_mode = True
             yield token
     yield 0x00
 
 
-def read_token(line:str, idx:int):
+def read_token(line: str, idx: int):
+    """Reads forward from idx and emits next matching token(s)."""
     for token in TOKENS:
         lookahead_idx = idx
         token_idx = 0
@@ -208,7 +126,7 @@ def read_token(line:str, idx:int):
 
             if char.upper() != token[token_idx]:
                 break
-            if token_idx == len(token)-1:
+            if token_idx == len(token) - 1:
                 token_match = True
                 break
 
@@ -219,23 +137,24 @@ def read_token(line:str, idx:int):
 
     if not token_match:
         # Didn't find one, next character must be a literal
-        return [ord(line[idx])], lookahead_idx+1
+        return [ord(line[idx])], lookahead_idx + 1
 
     # need to read one more char to disambiguate "AT/ATN/A TO"
     if token == "AT":
         if line[lookahead_idx + 1] == "N":
-            return [TOKENS["ATN"]], lookahead_idx+2
+            return [TOKENS["ATN"]], lookahead_idx + 2
         elif line[lookahead_idx + 1] == "O":
-            return [ord("A"), TOKENS["TO"]], lookahead_idx+2
-    return [TOKENS[token]], lookahead_idx+1
+            return [ord("A"), TOKENS["TO"]], lookahead_idx + 2
+    return [TOKENS[token]], lookahead_idx + 1
 
 
 def main(argv):
     sys.stdout.write("0800: ")
     with open(argv[1], "r") as f:
-        for c in parse_program(f.readlines()):
+        for c in tokenize_program(f.readlines()):
             sys.stdout.write("%02x " % c)
     print()
+
 
 if __name__ == "__main__":
     main(sys.argv)
